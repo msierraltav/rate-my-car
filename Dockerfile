449b6a3d50be3b8f6ef8031ba9c7c13120a1f4d4
msierraltav <115440917+msierraltav@users.noreply.github.com>
2025-03-05 22:47:59 -0700

    fix container

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
 FROM php:8.4-apache
+WORKDIR "/application"
 
-RUN apt-get update && apt-get install -y libpq-dev && docker-php-ext-install pdo pdo_pgsql
+# RUN apt-get update && apt-get install -y libpq-dev && docker-php-ext-install pdo pdo_pgsql
+
+RUN apt-get update \
+    && apt-get -y --no-install-recommends install \
+        libpq-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/* \
+    && docker-php-ext-install pdo pdo_pgsql pgsql
 
 COPY /src /var/www/html/
 
 COPY /config/config.ini /usr/local/etc/php/conf.d/
 
-EXPOSE 8080
+EXPOSE 80
--- src/conexion.php
+++ src/conexion.php
@@ -1,22 +1,21 @@
 <?php
   class Conexion {
 
-
     function ConexionDB() {
-
 
       $hosturl = getenv('POSTGRES_HOST');
       $hostport = getenv('POSTGRES_PORT');
 
-      $host = $hosturl . ":" . $hostport;
       $user = getenv('POSTGRES_USER');
       $password = getenv('POSTGRES_PASSWORD');
       $dbName = getenv('POSTGRES_DB');
       $dbPort = getenv('POSTGRES_PORT');
       $connexion = null;
 
+      $connectionString = "pgsql:host=$hosturl;port=$hostport;dbname=$dbName;user=$user;password=$password";
+      
       try{
-        $connexion = new PDO ("pgsql:host=".$hosturl."; dbname=".$dbName, $user, $password);
+        $connexion = new PDO ($connectionString);
         echo " Sucessfull connection";
       }
       catch (PDOException $error){
