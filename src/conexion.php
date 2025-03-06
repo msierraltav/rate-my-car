3417fc5e80b6f52cb7afa45a477c3ce75dfc0128
msierraltav <115440917+msierraltav@users.noreply.github.com>
2025-03-05 19:43:25 -0700

    postgres db

--- src/conexion.php
+++ src/conexion.php
@@ -1,16 +1,22 @@
 <?php
   class Conexion {
-    
-    function ConexionDB() {
-      $host = 'localhost';
-      $user = 'root';
-      $password = '';
-      $dbName = 'mydb';
 
+
+    function ConexionDB() {
+
+
+      $hosturl = getenv('POSTGRES_HOST');
+      $hostport = getenv('POSTGRES_PORT');
+
+      $host = $hosturl . ":" . $hostport;
+      $user = getenv('POSTGRES_USER');
+      $password = getenv('POSTGRES_PASSWORD');
+      $dbName = getenv('POSTGRES_DB');
+      $dbPort = getenv('POSTGRES_PORT');
       $connexion = null;
 
       try{
-        $connexion = new PDO ("pgsql:host=$host;dbname=$dbName", $user, $password);
+        $connexion = new PDO ("pgsql:host=".$hosturl."; dbname=".$dbName, $user, $password);
         echo " Sucessfull connection";
       }
       catch (PDOException $error){
